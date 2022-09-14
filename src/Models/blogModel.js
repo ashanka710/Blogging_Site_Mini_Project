247c3f81a1af0870662168344795212c42ce1978
ashanka710 <ashankad343@gmail.com>
2022-09-14 08:37:57 +0530

    done project 1

--- src/Models/blogModel.js
+++ src/Models/blogModel.js
@@ -1,50 +1,47 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
-const blogSchema = mongoose.Schema(
-    {
-        title: {
-            type: String,
-            required: true,
-            trim:true
+const blogSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
 
-        },
-        body: {
-            type: String,
-            required: true,
-            trim:true
-            
-        },
-        authorId: {
-            type: ObjectId,
-            refs: ' Author',
-            required: true,
-
-        },
-        tags: [String],
-        category: [{
-            type: String,
-            required: true,
-
-        }],
-        subcategory: [String],
-
-        deletedAt: {
-            type: Date,
-            default: null
-        },
-        isDeleted: {
-            type: Boolean,
-            default: false
-        },
-        publishedAt: {
-            type: Date,
-            default: null
-        },
-        isPublished: {
-            type: Boolean,
-            default: false
-        },
     },
-    { timestamps: true }
-)
+    body: {
+        type: String,
+        required: true,
+        trim: true
+
+    },
+    authorId: {
+        type: ObjectId,
+        refs: 'Author',
+        required: true,
+
+    },
+    tags: [String],
+    category: [{
+        type: String,
+        required: true,
+
+    }],
+    subcategory: [String],
+
+    deletedAt: {
+        type: Date,
+        default: null
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    publishedAt: {
+        type: Date,
+        default: null
+    },
+    isPublished: {
+        type: Boolean,
+        default: false
+    },
+}, { timestamps: true })
 module.exports = mongoose.model('Blogs', blogSchema)
